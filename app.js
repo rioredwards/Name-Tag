51d94986de93eebf7c5871c2308469330f6921dc
Lexus Banton <lexusbanton.kr@gmail.com>
2022-09-07 15:12:37 -0700

    It works!

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // Get HTML Elements
-const nameEntry = document.getElementById('textbox');
+const textBox = document.getElementById('textbox');
 const submitBtn = document.getElementById('submit-btn');
 const nameDisplay = document.getElementById('name-section');
-console.log(nameEntry);
+console.log(textBox);
 console.log(submitBtn);
 console.log(nameDisplay);
 
 submitBtn.addEventListener('click', () => {
-
+    const nameEntry = textBox.value;
+    console.log(nameEntry);
+    nameDisplay.textContent = nameEntry;
 });
